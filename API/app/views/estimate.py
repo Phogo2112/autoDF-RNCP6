718301e5626f54bfeccd5b1269b6b11b7eb178c0
Dain <sulydain85@gmail.com>
2026-01-12 22:16:28 +0100

    added serializers and models migrate and makemigrate

--- API/app/views/estimate.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from app.models import Estimate
-from app.serializers import EstimateSerializer
-
-class EstimateViewSet(viewsets.ModelViewSet):
-  queryset = Estimate.objects.all()
-  serializer_class = EstimateSerializer
--- API/app/views/home.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from app.models import Home
-from app.serializers import HomeSerializer
-
-class HomeViewSet(viewsets.ModelViewSet):
-  queryset = Home.objects.all()
-  serializer_class = HomeSerializer
